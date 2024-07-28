f3d0258b9302c56571af433b1fe75a3f0b830164
Haonan <guanhn1214@gmail.com>
2024-07-28 19:09:00 -0400

    Added SignupForm.js, FoodList.js, MyCart.js, updated App.js to integrated everything together

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import { Layout } from "antd";
+import { Layout, Typography } from "antd";
+import { useState } from "react";
+import LoginForm from "./components/LoginForm";
+import FoodList from "./components/FoodList";
+import MyCart from "./components/MyCart";
+import SignupForm from "./components/SignupForm";
 
 const { Header, Content } = Layout;
+const { Title } = Typography;
 
 function App() {
+    const [authed, setAuthed] = useState(false);
+
     return (
         <Layout style={{ height: "100vh" }}>
-            <Header style={{ color: "white" }}>header</Header>
+            <Header>
+                <div
+                    className="header"
+                    style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                    <Title
+                        level={2}
+                        style={{ color: "white", lineHeight: "inherit", marginBottom: 0 }}
+                    >
+                        GourmetHub
+                    </Title>
+                    <div>{authed ? <MyCart /> : <SignupForm />}</div>
+                </div>
+            </Header>
             <Content
                 style={{
                     padding: "50px",
@@ -13,7 +34,7 @@ function App() {
                     overflowY: "auto",
                 }}
             >
-                content
+                {authed ? <FoodList /> : <LoginForm onSuccess={() => setAuthed(true)} />}
             </Content>
         </Layout>
     );
